cd36a760c263ff27c76a46f6db5644c59961f0f1
rjgohar <rehmatjan143@gmail.com>
2022-11-29 09:38:29 +0500

    int signup

--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -5,12 +5,57 @@ import {
   Typography,
 } from "@material-ui/core";
 import TextField from "../input";
-import React from "react";
+import React, { useEffect } from "react";
 import Buttons from "../buttons";
-import { Link } from "react-router-dom";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { registerUser } from "../../redux/register/register.actions";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function SignupSection() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const {
+    isUserRegistering,
+    userResisgteringStatus,
+    isUserRegisteringSuccess,
+  } = useSelector((state) => state.registerSlice);
+  const naviagte = useNavigate();
+
+  useEffect(() => {
+    let timer;
+    if (isUserRegisteringSuccess) {
+      timer = setTimeout(() => {
+        naviagte("/");
+      }, 2000);
+    }
+    return () => clearTimeout(timer);
+  }, [isUserRegisteringSuccess]);
+
+  const initialValues = {
+    userName: "",
+    passowrd: "",
+    email: "",
+  };
+  const signUpSchema = yup.object({
+    userName: yup.string().required("user name is required"),
+    passowrd: yup.string().required("password is required").min(5),
+    email: yup.string().email("Invalid email format").required("Required"),
+  });
+  const formik = useFormik({
+    initialValues,
+    validationSchema: signUpSchema,
+    enableReinitialize: true,
+    onSubmit: async (values) => {
+      const payload = {
+        email: values.email,
+        username: values.userName,
+        password: values.passowrd,
+      };
+      dispatch(registerUser(payload));
+    },
+  });
 
   return (
     <Box mb={10} className={classes.mainContainer}>
@@ -25,7 +70,16 @@ export default function SignupSection() {
           {" "}
           userName
         </Typography>
-        <TextField name="userName" variant="outlined" placeholder="UserName" />
+        <TextField
+          name="userName"
+          variant="outlined"
+          placeholder="UserName"
+          value={formik.values.userName}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          helperText={formik.touched.userName && formik.errors.userName}
+          error={formik.touched.userName && Boolean(formik.errors.userName)}
+        />
       </Box>
       <Box pt={2} pb={3}>
         <Typography className={classes.typo} variant="body1">
@@ -35,7 +89,12 @@ export default function SignupSection() {
         <TextField
           variant="outlined"
           placeholder="email"
-          name="Email Address / NIC *"
+          name="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          helperText={formik.touched.email && formik.errors.email}
+          error={formik.touched.email && Boolean(formik.errors.email)}
         />
       </Box>
       <Box pt={2} pb={3}>
@@ -43,7 +102,16 @@ export default function SignupSection() {
           {" "}
           Password
         </Typography>
-        <TextField variant="outlined" placeholder="Password" name="passowrd" />
+        <TextField
+          variant="outlined"
+          placeholder="Password"
+          name="passowrd"
+          value={formik.values.passowrd}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          helperText={formik.touched.passowrd && formik.errors.passowrd}
+          error={formik.touched.passowrd && Boolean(formik.errors.passowrd)}
+        />
       </Box>
       <Typography
         style={{ color: "#fff", fontSize: "16px", textAlign: "center" }}
